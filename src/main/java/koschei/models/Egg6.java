512327053e7020b37cc39a447be3b836f65727fe
Ruslan Bagautdinov <ruslan.bagautdinov2000@mail.ru>
2024-11-25 17:02:50 +0500

    fixed

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,18 +1,18 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
 
-    private final Needle7 needle;
+    private Needle7 needle;
 
     @Autowired
-    public Egg6(Needle7 needle) {
+    public void setNeedle(Needle7 needle) {
         this.needle = needle;
     }
-
 
     @Override
     public String toString() {
